eabb3d617c4c5b6ac990f6532710a42c8dc50f8b
claviering <claviering@gmail.com>
2020-10-06 08:32:51 +0800

    init

---
+++ src/main/java/com/example/javaspringbootdemo/JavaspringbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringbootdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaspringbootdemo/JavaspringbootdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringbootdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
